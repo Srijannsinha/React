8e3dc8faebb08dd4c996757b569042ba6feb33d8
Srijansinha <srijansinha0123@gmail.com>
2025-01-04 12:03:38 +0530

    modifed app.js file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to see what I changed.
+          Edit <code>src/App.js</code> and save to see changes in the project.
         </p>
         <a
           className="App-link"
